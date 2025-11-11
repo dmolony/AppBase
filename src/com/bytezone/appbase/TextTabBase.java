7fda44016d1e07e7f00e5bfac3fe97fb368b9c69
Denis Molony <dmolony@users.noreply.github.com>
2025-11-11 12:56:44 +1000

    Added copyToClipboard()

--- src/com/bytezone/appbase/TextTabBase.java
+++ src/com/bytezone/appbase/TextTabBase.java
@@ -1,11 +1,22 @@
 package com.bytezone.appbase;
 
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 // -----------------------------------------------------------------------------------//
 public abstract class TextTabBase extends TabBase
 // -----------------------------------------------------------------------------------//
 {
+  protected final Clipboard clipboard = Clipboard.getSystemClipboard ();
+  protected final ClipboardContent content = new ClipboardContent ();
+  protected final TextFlow textFlow = new TextFlow ();
+  protected final ScrollPane scrollPane = new ScrollPane (textFlow);
+
   // ---------------------------------------------------------------------------------//
   public TextTabBase (String title, KeyCode keyCode)
   // ---------------------------------------------------------------------------------//
@@ -19,5 +30,20 @@ public abstract class TextTabBase extends TabBase
   // ---------------------------------------------------------------------------------//
   {
     valid = true;
+  }
+
+  // ---------------------------------------------------------------------------------//
+  public void copyToClipboard ()
+  // ---------------------------------------------------------------------------------//
+  {
+
+    StringBuilder text = new StringBuilder ();
+
+    for (Node node : textFlow.getChildren ())
+      if (node instanceof Text)
+        text.append (((Text) node).getText ());
+
+    content.putString (text.toString ());
+    clipboard.setContent (content);
   }
 }
